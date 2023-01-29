6a210249b00597493d8c717884ce9e9ee9ba787a
PhoenixNest <XenOmeGA@163.com>
2023-01-29 18:10:56 +0800

    [Add & Update] LoginActivity, *.gradle
    (1) Add a Login Activity to match the test manifest
    (2) Add comments to compile parameters

--- AWK-Gradle-Planet/app/src/main/java/io/dev/android/gradle/ui/main/MainActivity.kt
+++ AWK-Gradle-Planet/app/src/main/java/io/dev/android/gradle/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
-package io.dev.android.gradle
+package io.dev.android.gradle.ui.main
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -10,6 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.snackbar.Snackbar
+import io.dev.android.gradle.R
 import io.dev.android.gradle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
