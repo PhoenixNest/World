7795b89357fb18c917bc39c532a33657a440b568
PhoenixNest <XenOmeGA@163.com>
2023-02-03 15:56:41 +0800

    add project: AWK-Gradle-Planet
    Reference Link: [Jetpack Compose](https://developer.android.google.cn/courses/pathways/compose?hl=zh-cn)

---
+++ AWK-Jetpack-Composer/app/src/main/java/io/dev/android/composer/jetpack/model/Message.kt
@@ -0,0 +1,23 @@
+package io.dev.android.composer.jetpack.model
+
+data class Message(
+    val id: Int,
+    val user: User,
+    val content: String
+) {
+    companion object {
+        fun testConversation(): List<Message> {
+            val testList: MutableList<Message> = mutableListOf()
+            for (i in 1 until 10) {
+                if (((i - 1) % 2) != 0) {
+                    testList.add(Message(i, User.admin, "Warning message: you have reach Zone: $i, please exit right now !"))
+                    continue
+                }
+
+                testList.add(Message(i, User.admin, "Hey there, this is the test message which reference the id with $i"))
+            }
+
+            return testList
+        }
+    }
+}
---
+++ AWK-Jetpack-Composer/app/src/main/java/io/dev/android/composer/jetpack/viewmodel/MainViewModel.kt
@@ -0,0 +1,19 @@
+package io.dev.android.composer.jetpack.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.dev.android.composer.jetpack.model.Message
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    application: Application
+) : AndroidViewModel(application) {
+
+    var messageData: List<Message> = emptyList()
+
+    fun fetchMessageData() {
+        messageData = Message.testConversation()
+    }
+}
