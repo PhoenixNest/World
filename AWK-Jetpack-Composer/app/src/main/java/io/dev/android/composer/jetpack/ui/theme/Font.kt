2f047b20dc56f7213c15f22467587b9af956f544
PhoenixNest <XenOmeGA@163.com>
2023-02-09 15:29:52 +0800

    [Add & Update] Theme
    (1) Add Ui resources
    (2) Update App's Theme

---
+++ AWK-Jetpack-Composer/app/src/main/java/io/dev/android/composer/jetpack/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package io.dev.android.composer.jetpack.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import io.dev.android.composer.jetpack.R
+
+val fontUbuntu = FontFamily(
+    Font(R.font.ubuntu, FontWeight.Normal),
+    Font(R.font.ubuntu_medium, FontWeight.Medium),
+    Font(R.font.ubuntu_bold, FontWeight.Bold)
+)
