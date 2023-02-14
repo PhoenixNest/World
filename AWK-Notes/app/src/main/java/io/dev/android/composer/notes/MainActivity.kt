6e57dc2ea89eadcacf1875785f55ef2f9a4289db
PhoenixNest <XenOmeGA@163.com>
2023-02-14 10:46:09 +0800

    [Add] Notes app

---
+++ AWK-Notes/app/src/main/java/io/dev/android/composer/notes/MainActivity.kt
@@ -0,0 +1,42 @@
+package io.dev.android.composer.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import io.dev.android.composer.notes.ui.theme.AWKNotesTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AWKNotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AWKNotesTheme {
+        Greeting("Android")
+    }
+}
---
+++ AWK-Notes/app/src/main/java/io/dev/android/composer/notes/MyApplication.kt
@@ -0,0 +1,9 @@
+package io.dev.android.composer.notes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+}
