43a4cbde2d5128d6787c662491acdd9d9d75cd2f
PhoenixNest <XenOmeGA@163.com>
2023-02-14 18:57:42 +0800

    [Update] AWK-Notes
    (1) Move MainActivity to Ui layer

--- AWK-Notes/app/src/main/java/io/dev/android/composer/notes/feature/note/presentation/MainActivity.kt
+++ AWK-Notes/app/src/main/java/io/dev/android/composer/notes/feature/note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.dev.android.composer.notes
+package io.dev.android.composer.notes.feature.note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import io.dev.android.composer.notes.ui.theme.AWKNotesTheme
 
@@ -21,22 +18,9 @@ class MainActivity : ComponentActivity() {
             AWKNotesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AWKNotesTheme {
-        Greeting("Android")
     }
 }
