8a54fa30ae1ba1622cd96f040773aca24509bc81
PhoenixNest <46096203+PhoenixNest@users.noreply.github.com>
2024-01-29 10:54:41 +0800

    [Optimize] Project

--- Relic/app/src/main/java/io/dev/relic/feature/function/news/ui/NewsListPanel.kt
+++ Relic/app/src/main/java/io/dev/relic/feature/function/news/ui/NewsListPanel.kt
@@ -47,7 +47,6 @@ import io.core.ui.theme.mainBackgroundColor
 import io.core.ui.theme.mainButtonColorLightDark
 import io.core.ui.theme.mainTextColor
 import io.data.model.news.NewsArticleModel
-import io.data.model.news.NewsArticleModel.Companion.testList
 
 @Composable
 fun NewsListPanel(
@@ -283,7 +282,26 @@ private fun NewsCardFunctionBar(
 @Preview(showBackground = true, backgroundColor = 0xFF282C34)
 private fun NewsListPanelPreview() {
     NewsListPanel(
-        modelList = testList(),
+        modelList = listOf(
+            NewsArticleModel(
+                title = "Just this... and WoW will be perfect for me.",
+                subtitle = "(First of all, English is not my first language so forgive me if something is weird..) \\n \\nIt seems that the gypsy witch who stopped me one day while I was going to work and told me that you are all NPCs, this is just a simulation and the world conspires in my…",
+                author = "Fantazma",
+                thumbnailImageUrl = "null",
+                publishDate = "2023-11-28 T00:40:11Z",
+                contentUrl = "https://www.mmo-champion.com/threads/2644396-Just-this-and-WoW-will-be-perfect-for-me?p=54329863#post54329863",
+                source = "Mmo-champion.com"
+            ),
+            NewsArticleModel(
+                title = "House speaker suggest George Santos will quit rather than be expelled",
+                subtitle = "Speaker of the House Mike Johnson on Monday afternoon said he has spoken to U.S. Rep. George Santos, suggesting the embattled and indicted New York Republican might resign rather than face an impending expulsion vote he’s likely to lose.But over the holiday w…",
+                author = "David Badash, The New Civil Rights Movement",
+                thumbnailImageUrl = "https://www.rawstory.com/media-library/george-santos.jpg?id=32972710&width=1200&height=600&coordinates=0%2C25%2C0%2C25",
+                publishDate = "2023-11-27 T21:08:30Z",
+                contentUrl = "https://www.rawstory.com/house-speaker-suggest-george-santos-will-quit-rather-than-be-expelled/",
+                source = "Raw Story"
+            )
+        ),
         onCardClick = {},
         onLikeClick = {},
         onShareClick = {},
