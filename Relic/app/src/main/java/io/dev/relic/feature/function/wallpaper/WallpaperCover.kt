52091d6724c7668673d99af4c89111d74bc23df6
PhoenixNest <46096203+PhoenixNest@users.noreply.github.com>
2024-02-01 10:27:28 +0800

    [Added] WallpaperCover
    (1) Added the preview function

--- Relic/app/src/main/java/io/dev/relic/feature/function/wallpaper/WallpaperCover.kt
+++ Relic/app/src/main/java/io/dev/relic/feature/function/wallpaper/WallpaperCover.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.core.ui.CommonAsyncImage
 
@@ -21,4 +22,10 @@ fun WallpaperCover(
         imageHeight = screenHeight,
         imageShape = RectangleShape
     )
+}
+
+@Composable
+@Preview
+private fun WallpaperCoverPreview() {
+    WallpaperCover(imageUrl = "https://cdn.dribbble.com/users/78703/screenshots/4813446/attachments/1081953/weather_full.png")
 }
