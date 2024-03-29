904f7554a4939765b97709be86e5bc06905d5e56
Peter Chen <46096203+PhoenixNest@users.noreply.github.com>
2024-03-29 17:21:24 +0800

    [Optimize] Map module
    (1) Optimize the dependencies of map module

--- Relic/module/map/build.gradle.kts
+++ Relic/module/map/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     }
 
     kotlinOptions {
+        allWarningsAsErrors = true
         jvmTarget = "17"
     }
 
@@ -89,7 +90,9 @@ dependencies {
     /* ======================== Third-party Extension ======================== */
 
     // TomTom - Map display
-    implementation("com.tomtom.sdk.maps:map-display:0.50.6")
+    implementation("com.tomtom.sdk.maps:map-display:0.50.6"){
+        exclude(group = "com.google.protobuf")
+    }
     // TomTom - Location provider
     implementation("com.tomtom.sdk.location:provider-android:0.50.6")
     // TomTom - Map search online services
--- Relic/settings.gradle.kts
+++ Relic/settings.gradle.kts
@@ -1,3 +1,5 @@
+rootProject.name = "Relic"
+
 pluginManagement {
     repositories {
         google()
@@ -5,32 +7,37 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        jcenter()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
 
         // TomTom Map
-        maven { url = uri("https://repositories.tomtom.com/artifactory/maven") }
+        maven("https://repositories.tomtom.com/artifactory/maven") {
+            content { includeGroupByRegex("com\\.tomtom\\..+") }
+        }
 
         // Lottie
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
-rootProject.name = "Relic"
+
 // Extension module
 include(":module:ad")
 include(":module:map")
 include(":module:location")
 include(":agent:gemini")
+
 // Feature layer
 include(":app")
+
 // Domain layer
 include(":domain")
 include(":common")
+
 // Core layer
 include(":core:database")
 include(":core:datastore")
