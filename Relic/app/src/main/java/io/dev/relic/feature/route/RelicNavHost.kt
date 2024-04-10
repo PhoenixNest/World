f820f20a24b9961fcea304361b4804ff299085f4
Peter Chen <46096203+PhoenixNest@users.noreply.github.com>
2024-04-10 18:36:39 +0800

    [Add] Navigation animate

--- Relic/app/src/main/java/io/dev/relic/feature/route/RelicNavHost.kt
+++ Relic/app/src/main/java/io/dev/relic/feature/route/RelicNavHost.kt
@@ -1,5 +1,7 @@
 package io.dev.relic.feature.route
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -58,7 +60,31 @@ fun MainFeatureNavHost(
     NavHost(
         navController = navHostController,
         startDestination = startDestination,
-        modifier = modifier
+        modifier = modifier,
+        enterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(durationMillis = 350)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(durationMillis = 350)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(durationMillis = 350)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(durationMillis = 350)
+            )
+        }
     ) {
         pageHome(
             mainScreenState = mainScreenState,
