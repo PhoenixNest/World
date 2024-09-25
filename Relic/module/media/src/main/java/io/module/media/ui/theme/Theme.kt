211b310042f93c536d1f9da1d99b54187b8da282
Peter Chen <XenOmeGA@163.com>
2024-09-25 18:10:33 +0800

    [Update] Media module
    (1) Update ui

--- Relic/module/media/src/main/java/io/module/media/ui/theme/Theme.kt
+++ Relic/module/media/src/main/java/io/module/media/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package io.module.media.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
@@ -48,8 +47,6 @@ fun MediaModuleTheme(
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
