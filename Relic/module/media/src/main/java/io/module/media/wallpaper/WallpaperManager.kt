910548f1d5b56767bca199b6dfa53b2822c48643
Peter Chen <XenOmeGA@163.com>
2024-11-19 16:19:20 +0800

    [Add] Gallery
    (1) Added the basic logic of how to set current picture to wallpaper

--- Relic/module/media/src/main/java/io/module/media/wallpaper/WallpaperManager.kt
+++ Relic/module/media/src/main/java/io/module/media/wallpaper/WallpaperManager.kt
@@ -6,8 +6,10 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.provider.MediaStore
 import io.module.media.utils.MediaLogUtil
 import io.module.media.wallpaper.live.LiveWallpaperService
+import java.io.File
 import java.io.InputStream
 
 object WallpaperManager {
@@ -15,6 +17,22 @@ object WallpaperManager {
     private const val TAG = "WallpaperManager"
 
     /* ======================== Built-in tools function ======================== */
+
+    fun getCropAndSetWallpaperIntent(
+        context: Context,
+        imageUri: Uri
+    ): Intent {
+        return try {
+            WallpaperManager
+                .getInstance(context)
+                .getCropAndSetWallpaperIntent(imageUri)
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            Intent(WallpaperManager.ACTION_CROP_AND_SET_WALLPAPER)
+                .setDataAndType(imageUri, "image/*")
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+    }
 
     fun setBitmapWallpaper(
         context: Context,
@@ -81,5 +99,39 @@ object WallpaperManager {
                 putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, componentName)
             }
         )
+    }
+
+    /* ======================== Tools function ======================== */
+
+    fun getCacheImageUri(
+        context: Context,
+        bitmap: Bitmap,
+        saveDirName: String = "cache_wallpaper",
+        saveFileName: String = "cache_wallpaper"
+    ): Uri? {
+        return try {
+            val saveDir = context.cacheDir ?: null
+            val contentResolver = context.contentResolver ?: null
+            val saveDirFile = File(saveDir, saveDirName)
+
+            if (!saveDirFile.exists()) {
+                saveDirFile.mkdirs()
+            }
+
+            val currentSystemTime = System.currentTimeMillis()
+            val realSaveFileName = "${currentSystemTime}-${saveFileName}.png"
+            val saveImageFile = File(saveDirFile, realSaveFileName)
+            val saveImageUrl = MediaStore.Images.Media.insertImage(
+                /* cr = */ contentResolver,
+                /* source = */ bitmap,
+                /* title = */ saveImageFile.name,
+                /* description = */ null
+            ) ?: null
+
+            Uri.parse(saveImageUrl)
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            null
+        }
     }
 }
